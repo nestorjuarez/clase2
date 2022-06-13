5b8d6817800539b85fd1d6ad34c2be4ade7d0e1f
nestorjuarez <pruebanrj@hotmail.com>
2022-06-13 12:22:09 -0300

    nuevo navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = ()=>{
+    return (
+    <ul>
+        <li><a href="default.asp">Home</a></li>
+        <li><a href="news.asp">News</a></li>
+        <li><a href="contact.asp">Contact</a></li>
+        <li><a href="about.asp">About</a></li>
+    </ul>
+    )
+}
+
+export default NavBar;
